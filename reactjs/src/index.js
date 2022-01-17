e959f556655282fc48032b44c165801086df6880
marcosDeveloperr <marcos.ti.profissional@gmail.com>
2022-01-17 17:25:27 -0300

    primeiro JSX

--- reactjs/src/index.js
+++ reactjs/src/index.js
@@ -2,9 +2,25 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./styles.css"
 
+
+function primeiroJSX()
+{
+  return (<div>Marcos</div>)   
+  
+}   
+
+function soma(a,b)
+{
+  return a+b
+}
+
 const App = () => {
   return (
-    <div className="App">Ola, mundo reactJS...</div>
+    <div className="App">
+      <h3>Ola, mundo reactJS - {primeiroJSX()}</h3>
+      <p>Soma: {soma(3,10)}</p>    
+    </div>
+  
   )
 }
 
