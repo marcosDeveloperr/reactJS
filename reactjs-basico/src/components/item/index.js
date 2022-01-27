a03e4d7e5628f16023d86658df026fe9ca4109f3
marcosDeveloperr <marcos.ti.profissional@gmail.com>
2022-01-27 14:50:10 -0300

    using props

--- reactjs-basico/src/components/item/index.js
+++ reactjs-basico/src/components/item/index.js
@@ -1,6 +1,8 @@
-const Item = () => {
+//props - propriedade
+const Item = (props) => {
     return (
-        <a href="/" className="list-group-item list-group-item-action list-group-item-dark">Flamengo</a>
+        <a href="/" className="list-group-item list-group-item-action list-group-item-dark">{props.texto}</a>
+        //{props.texto} é atributo
     )
 }
 
