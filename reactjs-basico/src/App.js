cffe4d912c8df69bfb7c8329a8daf6e87ddfb2ff
marcosDeveloperr <marcos.ti.profissional@gmail.com>
2022-01-27 13:05:58 -0300

    add react-basico

---
+++ reactjs-basico/src/App.js
@@ -0,0 +1,14 @@
+const App = () => {
+  return(
+    <h1>Oi, Mundo reactJS</h1>
+  )
+}
+
+export default App;
+
+//**Outra forma correta**/
+// export default function App(){
+//   return(
+//     <h1>Oi, Mundo reactJS.</h1>
+//   )
+// }
