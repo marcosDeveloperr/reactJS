edf1e1bcf82f0d5abde1fb7e5152b389085e03b4
marcosDeveloperr <marcos.ti.profissional@gmail.com>
2022-01-27 13:59:41 -0300

    criando lista sem estilo

--- reactjs-basico/src/App.js
+++ reactjs-basico/src/App.js
@@ -1,6 +1,26 @@
 const App = () => {
   return(
-    <h1>Oi, Mundo reactJS</h1>
+    //nao permite trabalhar com elementos irmaos. 
+    //Para isso trabalhamos com esses elementos dentro de uma div.
+    // <h1>Oi, Mundo reactJS</h1>
+    // <p>Melhor time do mundo é o Flamengo.</p>
+
+    // <div>
+    //   <h1>Oi, Mundo reactJS</h1>
+    //   <p>Melhor time do mundo é o Flamengo.</p>
+    // </div>
+    //Ao invés de <div>...</div> também podemos usar <> .. </> 
+    <>
+      <h1>Oi, Mundo reactJS</h1>
+        <p>Melhor time do mundo é o Flamengo.</p>
+      <h2>Lista de times favoritos</h2>
+      <ul>
+        <li>Flamengo</li>
+        <li>Sport</li>
+        <li>Botafogo</li>
+        <li>Corinthians</li>
+      </ul>
+    </>
   )
 }
 
